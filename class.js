974a1ca18874009cfbb1b8f640d980d8db82dd5b
uday31 <udaybhatiya11@gmail.com>
2026-01-02 15:34:58 +0530

    Refactor HTML structure and improve navigation links

--- class.js
+++ class.js
@@ -1,24 +1,7 @@
-function subscribe() {
-  const email = document.getElementById('email').value;
-
-  if (email==="") {
-   alert("Please enter a valid email");
-  } else {
-   alert("thanks for subscribeing " + email);
-  }
-}
-
-function toggleMenu() {
-  const menu = document.querySelector("nav ul");
-  menu.classList.toggle("show");
-}
-
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-
-  if (window.scrollY > 20) {
-    header.style.boxShadow = "0 4px 15px rgba(0,0,0,0.4)";
-  } else {
-    header.style.boxShadow = "none";
-  }
+document.getElementById("contactBtn")?.addEventListener("click", function () {
+  window.location.href = "contact.html";
 });
+
+function goHome() {
+  window.location.href = "index.html";
+}
