da7a8d37b131e0019bd4215ff4cc09e9837b7029
uday31 <udaybhatiya11@gmail.com>
2026-01-03 11:44:06 +0530

    Remove class.css and refactor HTML structure for dynamic page loading

--- class.js
+++ class.js
@@ -1,24 +1,9 @@
-function add_section(...args) {
-    let section = document.createElement('div');
-    // console.log(args, typeof args);
-    for(i=0;i<args.length;i++){
-    // args.forEach(arg => {
-        section.innerHTML += args[i];
-        console.log(args[i]);
-    }
-    document.querySelector('#root').append(section);
+function loadPage(page) {
+    fetch(page + ".html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("root").innerHTML = data;
+        });
 }
 
-
-// dynamic loading of html files in pages
-pages = [['p1',['Upreach.html','uday.html']]];
-
-for(i=0;i<pages[0][1].length;i++){
-fetch("../HTML/".concat(pages[0][1][i]))
-    .then(response => response.text())
-    .then(data => {
-    //   document.getElementById("section").innerHTML = data;
-      add_section(data);
-    })
-    .catch(error => console.log("Error:", error));
-}
+loadPage("Upreach");
