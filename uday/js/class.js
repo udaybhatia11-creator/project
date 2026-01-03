448c02f604a58d7aa153af25b11ce80e92244839
uday31 <udaybhatiya11@gmail.com>
2026-01-03 12:41:17 +0530

    Refactor JavaScript structure: remove loadHTML function and update script references

---
+++ uday/js/class.js
@@ -0,0 +1,26 @@
+
+
+function add_section(...args) {
+    let section = document.createElement('div');
+    // console.log(args, typeof args);
+    for(i=0;i<args.length;i++){
+    // args.forEach(arg => {
+        section.innerHTML += args[i];
+        console.log(args[i]);
+    }
+    document.querySelector('#root').append(section);
+}
+
+
+// dynamic loading of html files in pages
+pages = [['p1',['section1_header.html','hello.html']]];
+
+for(i=0;i<pages[0][1].length;i++){
+fetch("../HTML/".concat(pages[0][1][i]))
+    .then(response => response.text())
+    .then(data => {
+    //   document.getElementById("section").innerHTML = data;
+      add_section(data);
+    })
+    .catch(error => console.log("Error:", error));
+}
