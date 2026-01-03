6e787e84807388244cb4523c294846f94be7088e
uday31 <udaybhatiya11@gmail.com>
2026-01-03 11:53:04 +0530

    Remove loadPage function and its invocation for cleaner code

--- class.js
+++
@@ -1,9 +0,0 @@
-function loadPage(page) {
-    fetch(page + ".html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("root").innerHTML = data;
-        });
-}
-
-loadPage("Upreach");
